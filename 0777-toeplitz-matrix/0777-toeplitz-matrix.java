40494c140c457e572d25c3d28ec3bf250df02f9b
ShaikHusnaThahera <shaikhusnathahera@gmail.com>
2025-07-30 19:07:05 +0530

    Time: 0 ms (100%), Space: 44.9 MB (8.54%) - LeetHub

---
+++ 0777-toeplitz-matrix/0777-toeplitz-matrix.java
@@ -0,0 +1,35 @@
+class Solution {
+      public boolean istop(int matrix[][],int y,int x){
+        int r=matrix.length;
+        int c=matrix[0].length;
+       int i= y;
+        int j=x;
+        int temp=matrix[i][j];
+        boolean valid=true;
+        while(i<r && j<c){
+            if(matrix[i][j]!=temp)
+                valid=false;
+            i++;
+            j++;
+            }
+              return valid;
+            }
+       public boolean isToeplitzMatrix(int[][] matrix) {
+       boolean ans=true;
+       int r=matrix.length;
+        int c=matrix[0].length;
+       for(int j=0;j<c;j++){
+        boolean temp=istop(matrix,0,j);
+        if(temp==false){
+            ans=false;
+        }
+       }
+       for(int i=1;i<r;i++){
+        boolean temp=istop(matrix,i,0);
+        if(!temp){
+            ans=false;
+        }
+       }
+       return ans;
+    }
+}
