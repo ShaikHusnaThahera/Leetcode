c7d78d1eef4b3b6341e0f9a758636ae6d9188f1d
ShaikHusnaThahera <shaikhusnathahera@gmail.com>
2025-09-10 14:49:45 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2248-minimum-cost-of-buying-candies-with-discount/2248-minimum-cost-of-buying-candies-with-discount.java
+++ 2248-minimum-cost-of-buying-candies-with-discount/2248-minimum-cost-of-buying-candies-with-discount.java
@@ -1,18 +1,14 @@
 class Solution {
     public int minimumCost(int[] cost) {
-        int ans=0;
+        int l=0,k=2,temp=0;
        Arrays.sort(cost);
-        for (int i = 0, j = cost.length - 1; i < j; i++, j--) {
-            int temp = cost[i];
-            cost[i] = cost[j];
-            cost[j] = temp;
+       for(int r=0;r<cost.length;r++){
+         temp+=cost[r];
+        if(r-l == k){
+            temp-=cost[l];
+            l+=3;
         }
-       for(int i=0;i<cost.length;i=i+3){
-           ans=ans+cost[i];
-           if(i+1<cost.length){
-               ans=ans+cost[i+1];
-           }
        }
-       return ans;
+       return temp;
     }
 }
