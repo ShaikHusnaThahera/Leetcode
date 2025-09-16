59917726225f2378f0b4b5695300e308ec56387b
ShaikHusnaThahera <shaikhusnathahera@gmail.com>
2025-09-16 22:10:07 +0530

    Time: 12 ms (73.42%), Space: 45.2 MB (21.52%) - LeetHub

--- 2134-maximize-the-confusion-of-an-exam/2134-maximize-the-confusion-of-an-exam.java
+++ 2134-maximize-the-confusion-of-an-exam/2134-maximize-the-confusion-of-an-exam.java
@@ -1,30 +1,24 @@
 class Solution {
-    public int maxConsecutiveAnswers(String answerKey, int k) {
-         int zcount=0,ocount=0,ans=0,l1=0,l2=0,res=0;
-     for(int r=0;r<answerKey.length();r++){
-        if(answerKey.charAt(r)=='T'){
-            zcount+=1;
-        }
-        while(zcount>k){
-            if(answerKey.charAt(l1)=='T'){
-                zcount--;
-            }
-            l1=l1+1;
-        }
-        ans=Math.max(ans,r-l1+1);
-     }
-     for(int r=0;r<answerKey.length();r++){
-        if(answerKey.charAt(r)=='F'){
-            ocount+=1;
-        }
-         while(ocount>k){
-            if(answerKey.charAt(l2)=='F'){
-                ocount--;
-            }
-            l2=l2+1;
-        }
-        res=Math.max(res,r-l2+1);
-     }
-     return Math.max(res,ans);
+    public int maxConsecutiveAnswers(String nums, int k) {
+         int cf=0,ct=0,l=0,ans=0;
+      for(int r=0;r<nums.length();r++){
+          if(nums.charAt(r)=='T'){
+              ct+=1;
+          }
+          else{
+              cf+=1;
+          }
+          while(Math.min(cf,ct)>k){
+              if(nums.charAt(l)=='T'){
+                  ct--;
+              }
+              else{
+                  cf--;
+              }
+              l++;
+          }
+          ans=Math.max(ans,r-l+1);
+      }
+      return ans;
     }
 }
