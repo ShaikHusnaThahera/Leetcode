cdf34c1c34cd39a9db9fc6af47ac777c5aadfe56
ShaikHusnaThahera <shaikhusnathahera@gmail.com>
2025-10-04 10:59:19 +0530

    Time: 23 ms (16.62%), Space: 58.8 MB (25.06%) - LeetHub

--- 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.java
+++ 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.java
@@ -3,7 +3,7 @@ class Solution {
         int ans=0;
         for(int i=0;i<bloomDay.length;i++){
             if(bloomDay[i]<=d){
-                ans+=1;
+                ans++;
             }
             else{
                 ans=0;
