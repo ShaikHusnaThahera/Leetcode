965230bbdb77c4d2ae14dd85c3910105c2162f1c
ShaikHusnaThahera <shaikhusnathahera@gmail.com>
2025-10-04 10:54:37 +0530

    Time: 23 ms (16.62%), Space: 58.1 MB (65.72%) - LeetHub

---
+++ 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.java
@@ -0,0 +1,39 @@
+class Solution {
+    public static boolean isPossible(int bloomDay[],int m,int k,int d){
+        int ans=0;
+        for(int i=0;i<bloomDay.length;i++){
+            if(d>=bloomDay[i]){
+                ans+=1;
+            }
+            else{
+                ans=0;
+            }
+            if(ans==k){
+                m-=1;
+                ans=0;
+            }
+            if(m==0){
+                return true;
+            }
+        }
+        return false;
+    }
+    public int minDays(int[] bloomDay, int m, int k) {
+        int tempr=(int)Math.pow(10,9);
+        int l=1;
+        int r=tempr;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(isPossible(bloomDay,m,k,mid)){
+                r=mid-1;
+            }
+            else{
+                l=mid+1;
+            }
+        }
+         if(l == tempr+1){
+           return -1;
+        }
+        return l;
+    }
+}
