f57917c7fc2dec589bc2f8c281e9877d40187376
ShaikHusnaThahera <shaikhusnathahera@gmail.com>
2026-03-08 19:29:06 +0530

    Time: 15 ms (24.04%), Space: 46.5 MB (32.63%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,4 +1,20 @@
 class Solution {
+    public boolean fun(HashMap<Character,Integer> hr1,HashMap<Character,Integer> hr2){
+            if(hr1.size()!=hr2.size()){
+                return false;
+            }
+            for(char key:hr1.keySet()){
+                if(!hr2.containsKey(key)){
+                    return false;
+                }
+                int a=hr1.get(key);
+                int b=hr2.get(key);
+                if(a!=b){
+                    return false;
+                }
+            }
+        return true;
+    }
     public boolean isAnagram(String s, String t) {
         HashMap<Character,Integer> hr1=new HashMap<>();
         HashMap<Character,Integer> hr2=new HashMap<>();
@@ -10,6 +26,6 @@ class Solution {
             char ch2=t.charAt(i);
             hr2.put(ch2,hr2.getOrDefault(ch2,0)+1);
         }
-     return hr1.equals(hr2);
+     return fun(hr1,hr2);
     }
 }
