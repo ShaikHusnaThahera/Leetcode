11c06e41645ddff1e7baf8f9434ee2e7da743b77
ShaikHusnaThahera <shaikhusnathahera@gmail.com>
2026-07-13 19:43:35 +0530

    Time: 14 ms (92.3%), Space: 51.2 MB (87.68%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+     Arrays.sort(g);
+      Arrays.sort(s);
+      int j=0,count=0;
+      for(int i=0;i<g.length&&j<s.length;){
+        if(s[j]>=g[i]){
+            count+=1;
+            j++;
+            i++;}
+            else{
+                j++;
+            }
+        
+      }   
+      return count;
+    }
+}
