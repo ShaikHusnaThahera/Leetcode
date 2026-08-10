b6a8201239b0bf42b6f9b1357881ece0f4c03fc9
ShaikHusnaThahera <shaikhusnathahera@gmail.com>
2026-08-10 15:12:54 +0530

    Time: 1 ms (99.95%), Space: 47.1 MB (28.4%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,16 +1,20 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int n=0;
-        HashMap<Integer,Integer> hr=new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            n=nums[i];
-            hr.put(n,hr.getOrDefault(n,0)+1);
+        // int n=0;
+        // HashMap<Integer,Integer> hr=new HashMap<>();
+        // for(int i=0;i<nums.length;i++){
+        //     n=nums[i];
+        //     hr.put(n,hr.getOrDefault(n,0)+1);
+        // }
+        // for(int h:hr.keySet()){
+        // if(hr.get(h)==1)
+        //       return h;
+        // }
+        // return 1;
+        int xor=nums[0];
+        for(int i=1;i<nums.length;i++){
+            xor=xor^nums[i];
         }
-        for(int h:hr.keySet()){
-        if(hr.get(h)==1)
-              return h;
-        }
-        return 1;
-        
+        return xor;
     }
 }
