d9d98c7e8be334b302f6203c677f7ac985d7b2cd
ShaikHusnaThahera <shaikhusnathahera@gmail.com>
2026-08-24 23:59:36 +0530

    Time: 1 ms (98.17%), Space: 47 MB (56.75%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution { 
+    public int[][] insert(int[][] intervals, int[] newInterval) { 
+        List<int[]> res = new ArrayList<>();
+        int i = 0; 
+        int n = intervals.length; 
+        
+        // 1. Add all intervals before the new interval
+        while (i < n && intervals[i][1] < newInterval[0]) { 
+            res.add(intervals[i]); 
+            i = i + 1; 
+        } 
+        
+        // 2. Merge all overlapping intervals with the new interval
+        while (i < n && intervals[i][0] <= newInterval[1]) { 
+            newInterval[0] = Math.min(newInterval[0], intervals[i][0]); 
+            newInterval[1] = Math.max(newInterval[1], intervals[i][1]); 
+            i += 1; 
+        } 
+        res.add(newInterval); 
+        
+        // 3. Add all remaining intervals
+        while (i < n) { 
+            res.add(intervals[i]); 
+            i = i + 1; 
+        } 
+        
+        // Convert the dynamic list back to a 2D array
+        return res.toArray(new int[res.size()][]); 
+    } 
+}
