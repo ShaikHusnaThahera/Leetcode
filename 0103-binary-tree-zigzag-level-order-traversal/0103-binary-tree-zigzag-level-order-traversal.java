990a63d9faaa79dd9fef0a94ee9c445196e73067
ShaikHusnaThahera <shaikhusnathahera@gmail.com>
2026-09-08 15:37:20 +0530

    Time: 1 ms (81.63%), Space: 43.7 MB (43.77%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -15,27 +15,29 @@
  */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        List<List<Integer>> ans =new ArrayList<>();
-        Queue<TreeNode> q=new LinkedList<>();
-        boolean flag=false;
-        if(root==null){
-            return ans;
-        }
-        q.offer(root);
-        while(!q.isEmpty()){
-            int size=q.size();
-            List<Integer> temp=new ArrayList<>();
-            for(int i=0;i<size;i++){
-                if(q.peek().left!=null)
+       List<List<Integer>> hr=new ArrayList<>();
+       Queue<TreeNode> q=new LinkedList<>();
+       q.offer(root);
+       boolean flag=false;
+       if(root==null){
+        return hr;
+       }
+       while(!q.isEmpty()){
+        int s=q.size();
+        ArrayList<Integer> temp=new ArrayList<>();
+        for(int i=0;i<s;i++){
+            if(q.peek().left!=null){
                 q.offer(q.peek().left);
-                if(q.peek().right!=null)
-                q.offer(q.peek().right);
-                temp.add(q.poll().val);
             }
-            flag=!flag;
-            if(!flag) Collections.reverse(temp);
-            ans.add(temp);
+            if(q.peek().right!=null){
+                q.offer(q.peek().right);
+            }
+            temp.add(q.poll().val);
         }
-        return ans;
+        flag=!flag;
+        if(!flag) Collections.reverse(temp);
+        hr.add(temp);
+       }
+       return hr;
     }
 }
